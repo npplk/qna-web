cadcd175c5539d564e5a2dd6d2f7b3783c0c426f
Ravinda Senarath <ravinda.senarath@cognite.com>
2022-06-15 22:48:19 +0530

    comment delete and create directory since we override

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,6 +1,6 @@
 
 #!/bin/bash
-rm -rf /home/ubuntu/npp-qna-web
-mkdir /home/ubuntu/npp-qna-web
+# rm -rf /home/ubuntu/npp-qna-web
+# mkdir /home/ubuntu/npp-qna-web
 cd /home/ubuntu/npp-qna-web
 npm install
