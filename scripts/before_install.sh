0327e0c1b2221d7d824e27f4189af319dca527b2
Ravinda Senarath <ravinda.senarath@cognite.com>
2022-06-20 23:16:25 +0530

    fix yarn install

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -9,5 +9,5 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install --lts
-npm install --global yarn
+npm install -g yarn
 yarn global add pm2
