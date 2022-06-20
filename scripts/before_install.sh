a1908a9d29a09b1202ff45b9c846665f93394ce5
Ravinda Senarath <ravinda.senarath@cognite.com>
2022-06-20 21:21:25 +0530

    remove node, npm installations from script since apt installations are old. Manually installed as per now. Yarn build introduced

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Install node.js and Forever.js
 apt-get update
-apt-get install nodejs -y
-apt-get install npm  -y
+# apt-get install nodejs -y
+# apt-get install npm  -y
 npm install forever -g
