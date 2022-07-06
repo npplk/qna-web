93dbfa0979430dca196726f73cb8a36966156132
Ravinda Senarath <ravinda.senarath@cognite.com>
2022-07-06 16:54:10 +0530

    nvm export for after install

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -6,4 +6,7 @@
 cd /home/ubuntu/npp-qna-web
 # yarn install
 # yarn run build
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads 
 npm install
