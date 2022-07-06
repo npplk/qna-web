77ed636f35b09d1b3f18aa8914aab8691349145e
Ravinda Senarath <ravinda.senarath@cognite.com>
2022-07-06 16:49:51 +0530

    delete app before

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Install node.js and Forever.js
-apt-get update
+# apt-get update
 # apt-get install nodejs -y
 # apt-get install npm  -y
-cd /home/ubuntu/npp-qna-web
+cd /home/ubuntu
+rm -rf npp-qna-web
 # curl -sL https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh -o install_nvm.sh
 # bash install_nvm.sh
 # export NVM_DIR="$HOME/.nvm"
