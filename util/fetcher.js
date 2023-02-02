2f601d2ea922f69e68236d92938af64eed2daf38
Nadeesh Peiris <nash.ucsc@gmail.com>
2023-02-02 17:56:46 +0530

    update server url

--- util/fetcher.js
+++ util/fetcher.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const baseURL =
   process.env.NODE_ENV === 'development'
     ? '/api' // http://localhost:8080
-    : `/api` // http://ec2-13-229-119-28.ap-southeast-1.compute.amazonaws.com
+    : `/api` // http://ec2-52-221-242-21.ap-southeast-1.compute.amazonaws.com/
 
 const publicFetch = axios.create({
   baseURL
