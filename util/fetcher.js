cd3b304db4a898c5c37f7a95f0ced380c6067246
Nadeesh Peiris <nash.ucsc@gmail.com>
2023-02-02 15:17:26 +0530

    vercel proxy server

--- util/fetcher.js
+++ util/fetcher.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 const baseURL =
   process.env.NODE_ENV === 'development'
-    ? 'http://localhost:8080/api'
-    : `http://${process.env.SITE_NAME}/api`
+    ? '/api' // http://localhost:8080
+    : `/api` // http://ec2-13-229-119-28.ap-southeast-1.compute.amazonaws.com
 
 const publicFetch = axios.create({
   baseURL
